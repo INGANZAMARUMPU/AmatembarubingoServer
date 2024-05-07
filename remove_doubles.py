6bd5a1e0c0c06038b30c64caaecbf48ade452eaf
INGANZAMARUMPU <jonkur96@gmail.com>
2024-05-07 23:10:03 +0200

    cominator without DB cleaning

--- remove_doubles.py
+++ remove_doubles.py
@@ -17,23 +17,23 @@ tables:list[models.Model] = [
 titles = []
 contents = []
 
-with open("db.csv", "+a") as file:
-    for table in tables:
-        table.objects.filter(date__lt=datetime.date(2024, 5, 7)).delete()
-        for coords in tqdm(table.objects.values('II_5_coordonnees').distinct()):
-            object = table.objects.filter(II_5_coordonnees = coords["II_5_coordonnees"]).first()
-            item = model_to_dict(object)
-            # wrongs = table.objects.filter(II_5_coordonnees=item["II_5_coordonnees"]).exclude(id=item.id).delete()
-            for key, value in item.items():
-                if key not in titles:
-                    titles.append(key)
-            content = list(titles)
-            for i, key in enumerate(content):
-                content[i] = item.get(key) or ""
-            contents.append(content)
-        break
-
-print(contents)
-                
-            
-
+print("TRAITEMENT")
+for table in tables:
+    table.objects.filter(date__lt=datetime.date(2024, 5, 7)).delete()
+    for coords in tqdm(table.objects.values('II_5_coordonnees').distinct()):
+        object = table.objects.filter(II_5_coordonnees = coords["II_5_coordonnees"]).first()
+        item = model_to_dict(object)
+        # wrongs = table.objects.filter(II_5_coordonnees=item["II_5_coordonnees"]).exclude(id=item.id).delete()
+        for key, value in item.items():
+            if key not in titles:
+                titles.append(key)
+        content = list(titles)
+        for i, key in enumerate(content):
+            content[i] = item.get(key) or ""
+        contents.append(content)
+    
+print("ENREGISTREMENT")
+with open("db.csv", "w") as file:
+    print("\t".join(titles), file=file)
+    for line in tqdm(contents):
+        print("\t".join(line), file=file)
