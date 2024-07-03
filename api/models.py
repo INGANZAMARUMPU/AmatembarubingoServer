2afdddf57312a4caf46142b80af554c4fef4f1f1
INGANZAMARUMPU <jonkur96@gmail.com>
2024-07-03 05:37:01 +0200

    idem

--- api/models.py
+++ api/models.py
@@ -417,7 +417,7 @@ class VillageCollinaire(models.Model):
 class CarteThematique(models.Model):
     id = models.AutoField(primary_key=True)
     theme = models.CharField(max_length=128)
-    carte = models.FieldFile(upload_to="cartes/")
+    carte = models.FileField(upload_to="cartes/")
     details = models.CharField(max_length=256, null=True, blank=True)
 
     def __str__(self) -> str:
